a488bb1c8fa671292ef5ae7726337ec63dfaca32
Izaak2 <izaak.git@gmail.com>
2021-10-16 13:45:09 +0200

    Left shift added

---
+++ operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    //AND
+    char x = 1, y = 2; // x = 0000 0001, y = 0000 0010
+
+    if(x&y) // 0000 0001 & 0000 0010 = 0000 0000
+        printf("x&y is TRUE\n");
+    else
+        printf("x&&y is FALSE\n");
+    
+    if(x&&y) // 0000 0001 && 0000 0010 = 0000 0011 (not decimal 0)
+        printf("x&&y is TRUE\n");
+    else
+        printf("x&&y is FALSE\n");
+
+    // left shifts
+    char var = 3; //0000 0011
+    printf("%d", var<<1);
+    return 0;
+}
