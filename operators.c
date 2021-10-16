ab5e23384dd081d990beb9e2f9e9a1d90dbcfc49
Izaak2 <izaak.git@gmail.com>
2021-10-16 14:11:52 +0200

    added XOR and some printfs

--- operators.c
+++ operators.c
@@ -4,7 +4,7 @@ int main()
 {
     //AND
     char x = 1, y = 2; // x = 0000 0001, y = 0000 0010
-
+    printf("x = %d, y = %d\n", x, y);
     if(x&y) // 0000 0001 & 0000 0010 = 0000 0000
         printf("x&y is TRUE\n");
     else
@@ -17,11 +17,14 @@ int main()
 
     // left shifts
     char var = 3; //0000 0011
-    printf("%d\n", var<<1); //0000 0110
+    printf("3 << 1 = %d\n", var<<1); //0000 0110
 
     // right shift
     var = 6; //0000 0110
-    printf("%d\n", var>>1); //0000 0011
+    printf("6 >> 1 = %d\n", var>>1); //0000 0011
+
+    // XOR ^
+    printf("x^y = %d\n", x^y); //0000 0011
 
     return 0;
 }
